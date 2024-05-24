f3873d3a8c5129f266c48c58c2fd238e8b9da9ad
Anas Chhilif <anaschhilif33@gmail.com>
2024-05-24 11:13:29 +0000

    modified build and run script

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 
-# Step 1: Build the Kotlin project using Gradle
-echo "Building the Kotlin project..."
-./gradlew build
 
-if [ $? -ne 0 ]; then
-  echo "Gradle build failed. Exiting..."
-  exit 1
-fi
+docker build -t budgeteer .
 
-echo "Gradle build completed successfully."
-
-# Step 2: Build the Docker image for the Kotlin application
-echo "Building the Docker image for the Kotlin application..."
-podman build -t budgeteer-api .
-
-if [ $? -ne 0 ]; then
-  echo "Docker image build failed. Exiting..."
-  exit 1
-fi
-
-echo "Docker image build completed successfully."
-
-# Step 3: Run the services using podman-compose
-echo "Starting the services with podman-compose..."
-podman compose up
-
-if [ $? -ne 0 ]; then
-  echo "Failed to start services with podman-compose. Exiting..."
-  exit 1
-fi
-
-echo "Services started successfully."
+docker compose up -d
