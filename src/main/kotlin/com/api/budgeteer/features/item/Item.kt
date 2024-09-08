c0b3119b0c5824cf38a2e2c085791f5985f0a141
Saad Beidouri <saad.beidouri@etu.unice.fr>
2024-09-08 14:17:41 +0200

    add Item approval

--- src/main/kotlin/com/api/budgeteer/features/item/Item.kt
+++ src/main/kotlin/com/api/budgeteer/features/item/Item.kt
@@ -1,6 +1,7 @@
 package com.api.budgeteer.features.item
 
 import com.api.budgeteer.core.entities.TraceableEntity
+import com.api.budgeteer.features.itemApproval.ItemApproval
 import com.api.budgeteer.features.users.User
 import jakarta.persistence.*
 
@@ -15,14 +16,26 @@ data class Item (
 
     @ManyToOne
     @JoinColumn(name = "user_id")
-    val user: User
-) : TraceableEntity(){
-    constructor(name: String, price: Double, quantity: Int, user: User) : this(0, name, price, quantity, user)
-    constructor() : this(0, "itemName", 0.0, 0, User())
+    val user: User,
 
-    fun toDTO() = ItemDTO(id, user.id, name, price, quantity)
+    @OneToMany(mappedBy = "item", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    var approvals: List<ItemApproval> = mutableListOf(),
+
+    var isApproved: Boolean = false // Fully approved by all users
+
+
+) : TraceableEntity(){
+    constructor(name: String, price: Double, quantity: Int, user: User) : this(0, name, price, quantity, user, mutableListOf(), false)
+    constructor() : this(0, "itemName", 0.0, 0, User(), mutableListOf(), false)
+
+    fun toDTO() = ItemDTO(id, user.id, name, price, quantity, isApproved)
+
+    fun checkApprovalStatus(): Boolean {
+        this.isApproved = approvals.all { it.approved }
+        return this.isApproved
+    }
 
     override fun toString(): String {
-        return "Item(id=$id, name='$name', price='$price', quantity='$quantity')"
+        return "Item(id=$id, name='$name', price='$price', quantity='$quantity', isApproved='$isApproved')"
     }
 }
--- src/main/kotlin/com/api/budgeteer/features/item/ItemDTO.kt
+++ src/main/kotlin/com/api/budgeteer/features/item/ItemDTO.kt
@@ -1,5 +1,3 @@
 package com.api.budgeteer.features.item
 
-data class ItemDTO(var id: Long?, var userId: Long, var name: String, var price: Double, var quantity: Int) {
-
-}
+data class ItemDTO(var id: Long?, var userId: Long, var name: String, var price: Double, var quantity: Int, var isApproved: Boolean = false)
