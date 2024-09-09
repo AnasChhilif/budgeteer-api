42305a39a113b20d9ade2fa82df16dc1cbc91b30
Anas Chhilif <anaschhilif33@gmail.com>
2024-09-09 07:46:06 +0000

    cleaner e2e testing

--- e2e.py
+++ e2e.py
@@ -1,36 +1,36 @@
 import requests
+import json
 
 # Define the base URL of your API
 BASE_URL = "http://localhost:8080"
 
 # Define your test cases
 test_cases = [
-    {"method": "GET", "endpoint": "/users", "expected": []},
-    {"method": "POST", "endpoint": "/users", "data": {"firstName": "test", "lastName": "test", "email": "testing@test.com"},
-        "expected": {"id": 1, "firstName": "test", "lastName": "test", "email": "testing@test.com"}},
-    {"method": "GET", "endpoint": "/users/1",
-        "expected": {"id": 1, "name": "Test User"}},
-    {"method": "PUT", "endpoint": "/users/1", "data": {"name": "Updated User"},
-        "expected": {"id": 1, "name": "Updated User"}},
-    {"method": "DELETE", "endpoint": "/users/1",
-        "expected": {"message": "User deleted"}},
+    {"title": "Test Case 1: Get all users", "method": "GET",
+        "endpoint": "/users", "expected": [], "expected_status": 200},
+    {"title": "Test Case 2: Create a new user", "method": "POST", "endpoint": "/users", "data": {"firstName": "test", "lastName": "test", "email": "testing@test.com"},
+     "expected": {"id": 1, "firstName": "test", "lastName": "test", "email": "testing@test.com", "residenceId": None}, "expected_status": 201},
+    {"title": "Test Case 3: Create another new user", "method": "POST", "endpoint": "/users", "data": {"firstName": "secondtehst", "lastName": "seconddtest", "email": "test@test.com"},
+     "expected": {"id": 2, "firstName": "secondtehst", "lastName": "seconddtest", "email": "test@test.com", "residenceId": None}, "expected_status": 201},
 ]
 
 # Function to make the request and compare the response
 
 
-def test_endpoint(method, endpoint, data=None, expected=None):
+def test_endpoint(title, method, endpoint, data=None, expected=None, expected_status=200):
     url = BASE_URL + endpoint
     response = requests.request(method, url, json=data)
 
     # Check the status code
-    assert response.status_code == 200, f"Expected 200 OK, got {
-        response.status_code}"
+    assert response.status_code == expected_status, f"{
+        title} - Expected {expected_status}, got {response.status_code}"
 
     # If we expect a response, check it
     if expected is not None:
-        assert response.json() == expected, f"Expected {
-            expected}, got {response.json()}"
+        assert response.json() == expected, f"{
+            title} - Expected {json.dumps(expected)}, got {json.dumps(response.json())}"
+
+    print(f"\033[92m{title} - Passed\033[0m")
 
 
 # Run the tests
